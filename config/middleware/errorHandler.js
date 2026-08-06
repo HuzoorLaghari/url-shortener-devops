22057fa21ead5aa66a59a2ec460f9c8bb947b80f
murraco <mauriurraco@gmail.com>
2026-08-06 15:17:14 -0300

    Migrate to ESLint flat config

--- config/middleware/errorHandler.js
+++ config/middleware/errorHandler.js
@@ -1,9 +1,7 @@
-// eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
   if (res.headersSent) {
     return next(err);
   }
-  // eslint-disable-next-line no-console
   console.error(err);
   return res.status(500).json({ error: 'Internal server error' });
 }
---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'coverage/**'],
+  },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+    },
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+  },
+  {
+    files: ['view/javascript/**/*.js'],
+    languageOptions: {
+      sourceType: 'script',
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+      },
+    },
+  },
+];
